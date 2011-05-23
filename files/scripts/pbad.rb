c85ae564d443b86454996a5dc6e26e623f7bf4b3
mh <mh@immerda.ch>
2011-05-23 18:02:02 +0200

    make it possible to pass multiple specfiles

--- files/scripts/pbad.rb
+++ files/scripts/pbad.rb
@@ -2,18 +2,22 @@
 
 ### config
 
-releases = [5]
-archs = ['i386','x86_64']
+def releases
+  [5]
+end
+def archs
+  ['i386','x86_64']
+end
 
 ### end fo config
 
-unless ARGV.length == 1
-  puts "Usage: pbad path/to/SOME.spec"
+if ARGV.length < 1
+  puts "Usage: pbad path/to/SOME.spec[ path/to/OTHER.spec]"
   exit 1
 end
 
-unless File.exist?(specfile = ARGV[0])
-  puts "Specfile does not exist!"
+unless ARGV.all?{|file| File.exist?(file) }
+  puts "Some Specfiles do not exist!"
   exit 1
 end
 
@@ -22,15 +26,15 @@ unless File.exist?(File.expand_path('~/.pbad.yml'))
   exit
 end
 require 'yaml'
-options = YAML.load(File.read(File.expand_path('~/.pbad.yml')))
-unless options['target_dir'] && File.exist?(options['target_dir'])
+@options = YAML.load(File.read(File.expand_path('~/.pbad.yml')))
+unless @options['target_dir'] && File.exist?(@options['target_dir'])
   puts "Set target_dir in ~/.pbad.yml to an existing directory"
   exit 1
 end
 
 require 'rubygems'
 require 'highline/import'
-pgppassphrase = ask("Enter password for GPG Key:  ") { |q| q.echo = "x" }
+@pgppassphrase = ask("Enter password for GPG Key:  ") { |q| q.echo = "x" }
 
 require 'systemu'
 
@@ -48,41 +52,45 @@ def print_error(str,stdout,stderr)
   exit 1
 end
 
-status, stdout, stderr = exec_command("rpmbuild -bs --nodeps #{specfile}")
-srpmfile = stdout.chomp.sub('Wrote: ','')
-
-unless (status == 0) && srpmfile =~ /^\// && File.exist?(srpmfile)
-  print_error("SRPM not builded correctly!",stdout,stderr)
-end
-puts "SRPM (#{File.basename(srpmfile)}) successfully builded."
-
-require 'fileutils'
-releases.each do |release|
-  archs.each do |arch|
-    puts "Building RPM (#{File.basename(srpmfile)}) for RHEL#{release} on #{arch}"
-    status, stdout, stderr = exec_command("/usr/bin/mock -r epel-#{release}-#{arch} rebuild #{srpmfile}")
-    if status == 0
-      puts "RPM (#{File.basename(srpmfile)}) successfully builded for RHEL#{release} on #{arch}"
-      Dir["/var/lib/mock/epel-#{release}-#{arch}/result/*.rpm"].each do |rpm| 
-        unless rpm =~ /src.rpm$/
-          status, stdout, stderr = exec_command("signrpm #{pgppassphrase} #{rpm}")
-          unless stdout =~ /Pass phrase is good\./
-            print_error("Error while signing package #{rpm}",stdout,stderr)
-          else
-            puts "Signed package #{rpm}"
-            unless rpm =~ /-debuginfo-.*rpm$/
-             FileUtils.cp(rpm,"#{options['target_dir']}/el#{release}-testing/#{arch}/")
+def build_rpm(specfile)
+  status, stdout, stderr = exec_command("rpmbuild -bs --nodeps #{specfile}")
+  srpmfile = stdout.chomp.sub('Wrote: ','')
+  
+  unless (status == 0) && srpmfile =~ /^\// && File.exist?(srpmfile)
+    print_error("SRPM not builded correctly!",stdout,stderr)
+  end
+  puts "SRPM (#{File.basename(srpmfile)}) successfully builded."
+  
+  require 'fileutils'
+  releases.each do |release|
+    archs.each do |arch|
+      puts "Building RPM (#{File.basename(srpmfile)}) for RHEL#{release} on #{arch}"
+      status, stdout, stderr = exec_command("/usr/bin/mock -r epel-#{release}-#{arch} rebuild #{srpmfile}")
+      if status == 0
+        puts "RPM (#{File.basename(srpmfile)}) successfully builded for RHEL#{release} on #{arch}"
+        Dir["/var/lib/mock/epel-#{release}-#{arch}/result/*.rpm"].each do |rpm| 
+          unless rpm =~ /src.rpm$/
+            status, stdout, stderr = exec_command("signrpm #{@pgppassphrase} #{rpm}")
+            unless stdout =~ /Pass phrase is good\./
+              print_error("Error while signing package #{rpm}",stdout,stderr)
             else
-              FileUtils.cp(rpm,"#{options['target_dir']}/el#{release}-testing/#{arch}-debuginfo/")
+              puts "Signed package #{rpm}"
+              unless rpm =~ /-debuginfo-.*rpm$/
+               FileUtils.cp(rpm,"#{@options['target_dir']}/el#{release}-testing/#{arch}/")
+              else
+                FileUtils.cp(rpm,"#{@options['target_dir']}/el#{release}-testing/#{arch}-debuginfo/")
+              end
             end
+          else
+            FileUtils.cp(rpm,"#{@options['target_dir']}/el#{release}/SRPMS/")
           end
-        else
-          FileUtils.cp(rpm,"#{options['target_dir']}/el#{release}/SRPMS/")
+          puts "Deloyed package #{rpm} into testing."
         end
-        puts "Deloyed package #{rpm} into testing."
+      else
+        print_error("Error while building SRPM (#{File.basename(srpmfile)}) for RHEL#{release} on #{arch}",stdout,stderr)
       end
-    else
-      print_error("Error while building SRPM (#{File.basename(srpmfile)}) for RHEL#{release} on #{arch}",stdout,stderr)
     end
   end
 end
+
+ARGV.each{|specfile| build_rpm(specfile) }
