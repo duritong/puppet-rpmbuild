c7dc1a35e86170b6883bf75a9fb8d318af151495
mh <mh@immerda.ch>
2011-12-22 19:19:17 +0100

    getting things from config file

--- files/scripts/p2stable.rb
+++ files/scripts/p2stable.rb
@@ -5,30 +5,35 @@ unless ARGV.length == 1
   exit 1
 end
 
-releases=['5']
-archs = [ 'i386','x86_64' ]
-
 unless File.exist?(File.expand_path('~/.pbad.yml'))
   puts "No configuration file found in ~/.pbad.yml"
   exit
 end
 require 'yaml'
-options = YAML.load(File.read(File.expand_path('~/.pbad.yml')))
-unless options['target_dir'] && File.exist?(options['target_dir'])
+@options = YAML.load(File.read(File.expand_path('~/.pbad.yml')))
+unless @options['target_dir'] && File.exist?(@options['target_dir'])
   puts "Set target_dir in ~/.pbad.yml to an existing directory"
   exit 1
+end
+
+def releases
+  @options['releases']
+end
+
+def archs
+  @options['archs']
 end
 
 require 'fileutils'
 releases.each do |release|
   archs.each do |arch|
-    Dir["#{options['target_dir']}/el#{release}-testing/#{arch}/#{ARGV[0]}*.rpm"].each do |rpm| 
+    Dir["#{@options['target_dir']}/el#{release}-testing/#{arch}/#{ARGV[0]}*.rpm"].each do |rpm|
       puts "Move #{rpm}"
-      FileUtils.mv(rpm,"#{options['target_dir']}/el#{release}/#{arch}/")
+      FileUtils.mv(rpm,"#{@options['target_dir']}/el#{release}/#{arch}/")
     end
-    Dir["#{options['target_dir']}/el#{release}-testing/#{arch}-debuginfo/#{ARGV[0]}*.rpm"].each do |rpm| 
+    Dir["#{@options['target_dir']}/el#{release}-testing/#{arch}-debuginfo/#{ARGV[0]}*.rpm"].each do |rpm|
       puts "Move #{rpm}"
-      FileUtils.mv(rpm,"#{options['target_dir']}/el#{release}/#{arch}-debuginfo/")
+      FileUtils.mv(rpm,"#{@options['target_dir']}/el#{release}/#{arch}-debuginfo/")
     end
   end
 end
